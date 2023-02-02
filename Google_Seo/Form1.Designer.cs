241f405af9a0f7311031f83a75c321c2cbf82e1a
laihopquangg1821050876 <quangoc0302@gmail.com>
2023-02-02 10:08:01 +0700

    first commit

---
+++ Google_Seo/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Google_Seo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnBlog = new System.Windows.Forms.Button();
+            this.dgvLinks = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.txtLinks = new System.Windows.Forms.TextBox();
+            this.btnAfter = new System.Windows.Forms.Button();
+            this.btnBefore = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLinks)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSearch.Location = new System.Drawing.Point(144, 7);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(287, 20);
+            this.txtSearch.TabIndex = 0;
+            this.txtSearch.DoubleClick += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(437, 6);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(73, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Tìm kiếm ";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            this.btnSearch.Enter += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(9, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "G";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(30, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "o";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(47, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "o";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(64, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "g";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(80, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "l";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(90, 3);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "e";
+            // 
+            // btnBlog
+            // 
+            this.btnBlog.BackColor = System.Drawing.Color.White;
+            this.btnBlog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBlog.Location = new System.Drawing.Point(683, 255);
+            this.btnBlog.Name = "btnBlog";
+            this.btnBlog.Size = new System.Drawing.Size(114, 31);
+            this.btnBlog.TabIndex = 7;
+            this.btnBlog.Text = "Đăng";
+            this.btnBlog.UseVisualStyleBackColor = false;
+            this.btnBlog.Click += new System.EventHandler(this.btnBlog_Click);
+            // 
+            // dgvLinks
+            // 
+            this.dgvLinks.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvLinks.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvLinks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvLinks.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvLinks.Location = new System.Drawing.Point(5, 44);
+            this.dgvLinks.Name = "dgvLinks";
+            this.dgvLinks.Size = new System.Drawing.Size(163, 258);
+            this.dgvLinks.TabIndex = 8;
+            this.dgvLinks.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLinks_CellClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.btnBlog);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtTitle);
+            this.groupBox1.Controls.Add(this.txtContent);
+            this.groupBox1.Controls.Add(this.txtLinks);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(184, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(809, 295);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bài viết ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Blue;
+            this.label10.Location = new System.Drawing.Point(716, 198);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 20);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Ảnh";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Blue;
+            this.label8.Location = new System.Drawing.Point(6, 128);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 20);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Nội Dung ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Blue;
+            this.label9.Location = new System.Drawing.Point(6, 80);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 20);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Tiêu đề";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Blue;
+            this.label7.Location = new System.Drawing.Point(6, 34);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Links";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTitle.Location = new System.Drawing.Point(91, 78);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(235, 26);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // txtContent
+            // 
+            this.txtContent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtContent.Location = new System.Drawing.Point(91, 126);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(572, 163);
+            this.txtContent.TabIndex = 0;
+            // 
+            // txtLinks
+            // 
+            this.txtLinks.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLinks.Location = new System.Drawing.Point(91, 32);
+            this.txtLinks.Name = "txtLinks";
+            this.txtLinks.Size = new System.Drawing.Size(572, 26);
+            this.txtLinks.TabIndex = 0;
+            // 
+            // btnAfter
+            // 
+            this.btnAfter.Location = new System.Drawing.Point(57, 308);
+            this.btnAfter.Name = "btnAfter";
+            this.btnAfter.Size = new System.Drawing.Size(51, 23);
+            this.btnAfter.TabIndex = 8;
+            this.btnAfter.Text = "Sau";
+            this.btnAfter.UseVisualStyleBackColor = true;
+            this.btnAfter.Click += new System.EventHandler(this.btnAfter_Click_1);
+            // 
+            // btnBefore
+            // 
+            this.btnBefore.Location = new System.Drawing.Point(5, 308);
+            this.btnBefore.Name = "btnBefore";
+            this.btnBefore.Size = new System.Drawing.Size(43, 23);
+            this.btnBefore.TabIndex = 8;
+            this.btnBefore.Text = "Trước";
+            this.btnBefore.UseVisualStyleBackColor = true;
+            this.btnBefore.Click += new System.EventHandler(this.btnBefore_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(118, 308);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(50, 23);
+            this.btnLoad.TabIndex = 8;
+            this.btnLoad.Text = "Tải lại ";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(1001, 344);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnBefore);
+            this.Controls.Add(this.btnAfter);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvLinks);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Quét link ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLinks)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnBlog;
+        private System.Windows.Forms.DataGridView dgvLinks;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtLinks;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnAfter;
+        private System.Windows.Forms.Button btnBefore;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ Google_Seo/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Keys = OpenQA.Selenium.Keys;
+using System.Data.SQLite;
+using System.Data.Entity;
+using HtmlAgilityPack;
+using Google_Seo.Model;
+using System.Security.Policy;
+using static System.Windows.Forms.LinkLabel;
+
+namespace Google_Seo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            btnAfter.Enabled = false;
+            btnBefore.Enabled = false;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Kết nối sqlite
+            SQLiteConnection conn = new SQLiteConnection("data source = C:\\Users\\LAI HOP QUANG\\Desktop\\Code Private\\Google_Seo\\Link.db");
+            conn.Open();
+            string query = "select * from linkSeo";
+            SQLiteCommand cmd = new SQLiteCommand(query, conn);
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dt = new DataTable();
+            //dgvExport.DataSource = dt;
+
+        }
+        //Mở chrome
+        ChromeDriver drive;
+
+        //Tìm kiếm 
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            drive = HideSeleniumDriver();
+
+            //IWebDriver drive = new ChromeDriver();
+
+            drive.Navigate().GoToUrl("https://www.google.com/");
+
+
+            var WebDrive = drive.FindElement(By.XPath("//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]"));
+            WebDrive.SendKeys(txtSearch.Text);
+            Thread.Sleep(1000);
+            WebDrive.SendKeys(Keys.Enter);
+            //var data = drive.excuseScript("");
+            //Lấy link
+            var linkks = drive.Url;
+            string url = linkks;
+            try
+            {
+                var web = new HtmlWeb();
+                var doc = web.Load(url);
+                var links = doc.DocumentNode.Descendants("a")
+                     .Select(a => a.Attributes["href"].Value)
+                     .Where(link => link.Contains("http"))
+                     .ToList();
+                dgvLinks.DataSource = links.Select(link => new { Link = link }).ToList();
+            }
+            catch
+            {
+            }
+            btnAfter.Enabled = true;
+            btnBefore.Enabled = false;
+        }
+        //Đăng blog
+        private void btnBlog_Click(object sender, EventArgs e)
+        {
+
+            IWebDriver drvBlog = new ChromeDriver();
+            drvBlog.Navigate().GoToUrl("https://www.blogger.com/");
+            try
+            {
+                var Blog = drvBlog.FindElement(By.XPath("/html/body/header/div[1]/div[2]/a[1]"));
+                Blog.Click();
+                Thread.Sleep(2000);
+                Blog = drvBlog.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
+                Blog.Click();
+                Blog = drvBlog.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
+                Blog.SendKeys("quangoc0302@gmail.com");
+                Thread.Sleep(2000);
+                Blog = drvBlog.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button"));
+                Blog.Click();
+                Thread.Sleep(2000);
+            }
+            catch
+            {
+
+            }
+
+        }
+        // Ẩn chrome
+        private ChromeDriver HideSeleniumDriver()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+
+            ChromeDriver driver = new ChromeDriver(service, chromeOptions);
+            return driver;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+       
+
+
+        //Click vào dòng trong datagridview r đưa ra text
+        private void dgvLinks_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            DataGridViewRow row = dgvLinks.Rows[rowIndex];
+            string cellValue = row.Cells[0].Value.ToString();
+            txtLinks.Text = cellValue;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            dgvLinks.DataSource = null;
+            txtSearch.Clear();
+            btnAfter.Enabled = false;
+            btnBefore.Enabled = false;
+        }
+
+        private void btnAfter_Click_1(object sender, EventArgs e)
+        {
+            var Next = drive.FindElement(By.XPath("//*[@id=\"pnnext\"]/span[2]"));
+            Next.Click();
+            Thread.Sleep(1000);
+            dgvLinks.DataSource = null;
+            var linkks = drive.Url;
+            string url = linkks;
+            try
+            {
+                var web = new HtmlWeb();
+                var doc = web.Load(url);
+                var links = doc.DocumentNode.Descendants("a")
+                     .Select(a => a.Attributes["href"].Value)
+                     .Where(link => link.Contains("http"))
+                     .ToList();
+                dgvLinks.DataSource = links.Select(link => new { Link = link }).ToList();
+            }
+            catch
+            {
+            }
+            btnBefore.Enabled = true;
+        }
+
+        private void btnBefore_Click(object sender, EventArgs e)
+        {
+            var BackWeb = drive.FindElement(By.XPath("//*[@id=\"pnprev\"]/span[2]"));
+            BackWeb.Click();
+            Thread.Sleep(1000);
+            var linkks = drive.Url;
+            string url = linkks;
+            try
+            {
+                var web = new HtmlWeb();
+                var doc = web.Load(url);
+                var links = doc.DocumentNode.Descendants("a")
+                     .Select(a => a.Attributes["href"].Value)
+                     .Where(link => link.Contains("http"))
+                     .ToList();
+                dgvLinks.DataSource = links.Select(link => new { Link = link }).ToList();
+            }
+            catch
+            {
+            }
+
+
+        }
+    }
+}
